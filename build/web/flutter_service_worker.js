5ebfffbfaae0fecb6534815dcde92c220fe6bef9
jaronimoo <jarontolentino@gmail.com>
2024-03-30 08:34:55 -0700

    Release updates

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -12,9 +12,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "e39ebcd3022e4d8b3f96d92f4b139e0d",
-"/": "e39ebcd3022e4d8b3f96d92f4b139e0d",
-"main.dart.js": "7c46eb2c6b27debc47591d99e6b1fba8",
+"index.html": "48568d2604c879befc11946389822dfd",
+"/": "48568d2604c879befc11946389822dfd",
+"main.dart.js": "f78440285a70d64dde165c0614b6d308",
 "manifest.json": "7b87acc53311280e6558a777b2ce7f1d",
 "version.json": "50a026aa58c2c63e579d97a2e24dc47b"
 };
--- build/web/main.dart.js
+++ build/web/main.dart.js
@@ -47626,7 +47626,7 @@ F.SN.prototype={
 $1:function(a){return new F.iv("Do something!",null)},
 $S:95}
 F.kc.prototype={
-aN:function(){return new F.tQ([],H.b(["Nick's Grill","Mi Tierra","La Corona Mexican","burritos","Hokkaido Ramen Santouka","Shake Shack","Pho Ready","Pagliacci","Box Eatery","Hapa Food truck","Ezell's","Taco Time","Costco","teriyaki","sushi","ramen","Woodinville Ramen","sandwiches","Homegrown","pizza","burgers","steak and rice","chicken and rice","eggs","tacos!","Chinese","Italian","Padria","hot dogs","stir-fry","Shake and Go","Chick Fil A","thai","Indian food","tikka masala","new food","fish n chips","pasta primavera","chicken parm","gyro","apple sauce","noodles","Panda Express","salmon"],t.s),C.m)}}
+aN:function(){return new F.tQ([],H.b(["Nick's Grill","Mi Tierra","La Corona Mexican","burritos","Hokkaido Ramen Santouka","Shake Shack","Pho Ready","Pagliacci","Box Eatery","Ezell's","Taco Time","Costco","teriyaki","sushi","ramen","Woodinville Ramen","sandwiches","Homegrown","pizza","burgers","steak and rice","chicken and rice","eggs","tacos!","Chinese","Italian","Padria","hot dogs","stir-fry","Shake and Go","Chick Fil A","thai","Indian food","tikka masala","new food","fish n chips","pasta primavera","chicken parm","gyro","apple sauce","noodles","Panda Express","salmon"],t.s),C.m)}}
 F.tQ.prototype={
 ks:function(){var s=0,r=P.a5(t.z),q=this,p,o
 var $async$ks=P.a0(function(a,b){if(a===1)return P.a2(b,r)
@@ -47654,7 +47654,7 @@ r=t.p
 return M.a4h(o,T.vU(T.a2D(H.b([new T.d_(new V.aL(30,30,30,30),L.B2(n,K.bP(b).as.d,C.eA),p),D.xy(p,U.a3d(new L.lg("images/il_340x270.526781863_4j86.jpg")),C.bb,!1,p,p,p,p,p,p,p,p,p,p,s,p,p,p,p,p)],r),C.m9),p,p),T.rj(C.h4,H.b([new T.d_(new V.aL(15,0,0,0),new T.cR(C.nI,p,p,E.i5(L.ia(C.lb),!1,q.gqw()),p),p),new T.cR(C.kI,p,p,E.i5(L.ia(C.vd),!1,s),p),new T.d_(new V.aL(0,0,15,0),new T.cR(C.ip,p,p,E.i5(L.ia(C.oJ),!1,q.gLR()),p),p),new T.d_(new V.aL(0,0,85,0),new T.cR(C.ip,p,p,E.i5(L.ia(C.oK),!1,new F.W2(b)),p),p)],r),C.fY),C.oe)}}
 F.W0.prototype={
 $0:function(){var s,r=this.a;++r.d
-s=r.r[C.oj.CV(44)]
+s=r.r[C.oj.CV(43)]
 r.e=s
 r.f.push(s)},
 $S:0}
@@ -47674,7 +47674,7 @@ F.W1.prototype={
 $1:function(a){return new F.iv("Do Something!",null)},
 $S:95}
 F.iv.prototype={
-aN:function(){return new F.u2([],H.b(["Play basketball","Make a video","Play outside","Do some coloring","Ride bikes","Go for a walk","Go to the track","Play with air fort","Play with slime","Play toss the dounts","Make a fire","Play Roblox","Play Wii","Play with jump rope","Go to park","Play with MagTiles"],t.s),C.m)}}
+aN:function(){return new F.u2([],H.b(["Play basketball","Make a video","Play outside","Do some coloring","Ride bikes","Go for a walk","Go to the track","Play with the air fort","Play with slime","Play toss the dounts","Make a fire","Play Roblox","Play Wii","Play with jump rope","Go to park","Play with MagTiles","Play with cards","Read some books","Pester your parents","Picnic outside","Play at Abigail's house"],t.s),C.m)}}
 F.u2.prototype={
 bc:function(){this.n2()},
 kv:function(){var s=0,r=P.a5(t.z),q=this,p,o
@@ -47702,7 +47702,7 @@ r=t.p
 return M.a4h(o,T.vU(T.a2D(H.b([new T.d_(new V.aL(30,30,30,30),L.B2(n,K.bP(b).as.d,C.eA),p),D.xy(p,U.a3d(new L.lg("images/il_340x270.526781863_4j86.jpg")),C.bb,!1,p,p,p,p,p,p,p,p,p,p,s,p,p,p,p,p)],r),C.m9),p,p),T.rj(C.h4,H.b([new T.d_(new V.aL(15,0,0,0),new T.cR(C.nI,p,p,E.i5(L.ia(C.lb),!1,q.gqw()),p),p),new T.d_(new V.aL(0,0,15,0),new T.cR(C.ip,p,p,E.i5(L.ia(C.va),!1,q.gMd()),p),p),new T.d_(new V.aL(0,0,85,0),new T.cR(C.ip,p,p,E.i5(L.ia(C.oK),!1,s),p),p),new T.cR(C.kI,p,p,E.i5(L.ia(C.vb),!1,new F.Wb(b)),p)],r),C.fY),C.oe)}}
 F.Wa.prototype={
 $0:function(){var s,r=this.a;++r.d
-s=r.r[C.oj.CV(16)]
+s=r.r[C.oj.CV(21)]
 r.e=s
 r.f.push(s)},
 $S:0}
